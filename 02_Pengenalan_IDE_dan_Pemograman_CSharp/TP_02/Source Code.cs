4211af17375d6bd9011af399756dc42fb5595579
Rhystalgie <aufamuh12@gmail.com>
2025-03-03 16:43:14 +0700

    Pengerjaan TP

---
+++ 02_Pengenalan_IDE_dan_Pemograman_CSharp/TP_02/Source Code.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine(); 
+
+        if ("AIUEO".Contains(huruf))
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+        }
+
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
