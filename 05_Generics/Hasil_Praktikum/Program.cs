4484e4e1a2c347ebc5b9e082f9e66c1e064989a0
Rhystalgie <aufamuh12@gmail.com>
2025-03-11 12:47:28 +0700

    Pengumpulan Hasil Praktikum

---
+++ 05_Generics/Hasil_Praktikum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 2, 3, 4 };
+            List<int> list = new List<int>();
+            for (int x = 5; x < 10; x++)
+            {
+                list.Add(x);
+            }
+
+            ProcessItems<int>(arr);
+            ProcessItems<int>(list);
+        }
+
+        static void ProcessItems<T>(IList<T> coll)
+        {
+            // IsReadOnly return True for the array and False for the list
+            System.Console.WriteLine("IsReadOnly");
+
+            // The following statement causes a run-time exception for the array, but not for the list.
+            //coll.RemoveAt(4);
+
+            foreach (T item in coll)
+            {
+                System.Console.Write(item.ToString() + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
